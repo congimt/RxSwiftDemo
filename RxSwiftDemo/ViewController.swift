2896683dfbbd71bd8cb2246d1b16d3179fec89e1
Cong Nguyen <congnt@imt-soft.com>
2017-07-14 11:04:55 +0700

    edgdg

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -20,20 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //MARK: -------- 
-    
-    //MARK: -------- IBOutlet
-    
-    
-    //MARK: -------- Variable
-    
-    //MARK: -------- The App life cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.setupUI()
-        
-    }
+
+
     //MARK: -------- IBAction
     
     //MARK: -------- Setup Interface
